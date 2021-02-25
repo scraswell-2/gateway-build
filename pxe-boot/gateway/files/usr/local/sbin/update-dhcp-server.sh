aefef727011c6063ce099a97a3e182fe66c3742a
Sean Craswell <scraswell@gmail.com>
2021-02-25 11:27:01 -0400

    Added DHCP server update script.

---
+++ pxe-boot/gateway/files/usr/local/sbin/update-dhcp-server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+UPDATE_ISOS=${1}
+
+GIT_URL="https://github.com/scraswell-2/gateway-build.git"
+RECONF_DIR="/opt/system-reconfiguration"
+SYSTEM_SETUP_DIR="/opt/system-setup"
+
+git clone "${GIT_URL}" "${RECONF_DIR}" || { echo "Failed to clone repo."; exit 1; }
+
+${RECONF_DIR}/pxe-boot/post-install.sh gateway 1
+${SYSTEM_SETUP_DIR}/lib/200_setup_dhcpd.sh
+
+systemctl restart isc-dhcp-server.service
+
+rm -rf "${RECONF_DIR}" "${SYSTEM_SETUP_DIR}"
