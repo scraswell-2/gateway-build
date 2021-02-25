89dc6fa4d1b905effd92cd54c61d33ac07c17fb8
Sean Craswell <scraswell@gmail.com>
2021-02-25 13:02:56 -0400

    Added gitlab configuration files.

--- pxe-boot/gitlab/lib/400_install_gitlab.sh
+++ pxe-boot/gitlab/lib/400_install_gitlab.sh
@@ -14,4 +14,18 @@ apt -y install curl openssh-server ca-certificates perl
 
 curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | bash
 
-EXTERNAL_URL="http://gitlab.at.home" apt install -y gitlab-ce
+EXTERNAL_URL="https://gitlab.at.home" apt install -y gitlab-ce
+
+mkdir -pv /etc/gitlab/ssl
+
+curl -L -o /etc/gitlab/ssl/gitlab.at.home.crt "http://gateway.at.home/certs/gitlab.at.home.crt"
+curl -L -o /etc/gitlab/ssl/gitlab.at.home.key "http://gateway.at.home/certs/gitlab.at.home.key"
+
+touch /etc/gitlab/.not_configured
+
+install -D ${DIR}/../files/etc/systemd/system/gitlab-first-run.service \
+    /etc/systemd/system/gitlab-first-run.service
+
+install -D -m 0744 ${DIR}/../files/usr/local/sbin/gitlab-first-run \
+    /usr/local/sbin/gitlab-first-run
+
