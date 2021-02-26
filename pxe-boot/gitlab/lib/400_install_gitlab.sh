3fc82157bd8271209b81df6fc449eb0f3fc53de6
Sean Craswell <scraswell@gmail.com>
2021-02-26 11:28:55 -0400

    Added default runner.

--- pxe-boot/gitlab/lib/400_install_gitlab.sh
+++ pxe-boot/gitlab/lib/400_install_gitlab.sh
@@ -12,7 +12,22 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 apt -y install curl openssh-server ca-certificates perl
 
-curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | bash
+# add CE repo
+curl -sS -L "https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh" | bash
+
+# add Runner repo
+curl -sS -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh" | bash
+
+cat <<EOF | sudo tee /etc/apt/preferences.d/pin-gitlab-runner.pref
+Explanation: Prefer GitLab provided packages over the Debian native ones
+Package: gitlab-runner
+Pin: origin packages.gitlab.com
+Pin-Priority: 1001
+EOF
+
+apt -y update
+apt -y install gitlab-runner
+systemctl enable gitlab-runner.service
 
 mkdir -pv /etc/gitlab/ssl
 
