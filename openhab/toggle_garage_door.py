afcb078440d5c27ebc97d11604aecb89c09084de
Sean Craswell <scraswell@gmail.com>
2021-03-02 09:47:40 -0400

    Fixed garage door.

--- openhab/toggle_garage_door.py
+++ openhab/toggle_garage_door.py
@@ -8,6 +8,6 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(2,GPIO.OUT)
-GPIO.output(2,GPIO.HIGH)
-time.sleep(1)
 GPIO.output(2,GPIO.LOW)
+time.sleep(0.5)
+GPIO.output(2,GPIO.HIGH)
