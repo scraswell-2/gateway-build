e72123f3bfd56cadf3a2050e03ab86afb4c5b5db
Sean Craswell <scraswell@gmail.com>
2021-03-04 17:55:25 -0400

    Added services to KVM.

--- pxe-boot/kvm-host/lib/202_setup_avahi.sh
+++ pxe-boot/kvm-host/lib/202_setup_avahi.sh
@@ -15,9 +15,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ##############################################
 apt -y install \
         --no-install-recommends \
-        qemu-kvm \
-        ovmf \
-        cifs-utils
+        avahi
 ##############################################
 ## end install kvm
 ##############################################
